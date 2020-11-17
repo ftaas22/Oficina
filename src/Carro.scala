9bed5429c3a94fcfed65e101886005183b2b0193
joaob <joaobranco3000@gmail.com>
2020-11-17 22:43:44 +0000

    duas classes

---
+++ src/Carro.scala
@@ -0,0 +1,16 @@
+class Carro(modelo: String, ano: String,trabalho: Trabalho ) {
+
+
+  def getModel():String={
+    return modelo
+  }
+
+  def getAno():String= {
+    return ano
+  }
+
+  def getTrabalho():Trabalho= {
+    return trabalho
+  }
+
+}
