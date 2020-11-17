ef550e39ae103b294b22e58844655fcb5c0fbfb5
joaob <joaobranco3000@gmail.com>
2020-11-17 23:17:50 +0000

    System;mecanico;Trabalho

--- src/System.scala
+++ src/System.scala
@@ -1,9 +1,7 @@
 import scala.io.Source
 class System {
 
-
-
-
+}
   object Demo {
     def main(args: Array[String]) {
       val source:Iterator[String] = Source.fromFile("src\\carros.txt").getLines()
@@ -13,4 +11,3 @@ class System {
     }
   }
 
-}
