67675411cca67d12c3337179f39573c645d2a23d
joaob <joaobranco3000@gmail.com>
2020-11-18 16:36:08 +0000

    so falta a parte do trabalho do carro

--- src/Teste.scala
+++ src/Teste.scala
@@ -1,9 +1,37 @@
+import Avaria._
+
 import scala.io.Source
+import scala.util.control.Breaks.break
 
 object Teste{
   def main(args: Array[String]) {
-    val source:Iterator[String] = Source.fromFile("src\\carros.txt").getLines()
-    source.foreach(println)
+    var source:Iterator[String] = Source.fromFile("src\\carros.txt").getLines()
+   // var list:Array[Carro]
+    var str:String = "ESCAPE"
+    def find_Avaria( text:String): Unit = {
+      var a = Avaria.values.iterator
+      for (z <- a) {
+        if (z.toString.contains(text.toUpperCase)) {
+          print(z)
+          return z
+        }
+      }
+
+    }
+
+
+
+
+
+    for(i<-source){
+      //println(i.split(" ")(0))
+      //println(i.split(" ")(1))
+      //println(i.split(" ")(2))
+      var tra:Trabalho=new Trabalho(Avaria.BATERIA )
+
+      //var car: Carro = new Carro(i.split(" ")(0),i.split(" ")(1),)
+    }
+
 
   }
 
