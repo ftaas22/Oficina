a2dc1950884834beedaa01ffbdfc18eb667b69d9
djandrevilela <74112164+djandrevilela@users.noreply.github.com>
2020-12-07 18:41:21 +0000

    GUI part1

---
+++ src/MainWindowController.scala
@@ -0,0 +1,35 @@
+import javafx.fxml.{FXML, FXMLLoader}
+import javafx.scene.{Parent, Scene}
+import javafx.scene.control.Button
+import javafx.stage.{Modality, Stage}
+
+class MainWindowController {
+
+  @FXML
+  var mecanicos: Button = _
+  @FXML
+  var clientes: Button = _
+
+  def OnMecanicosClicked(): Unit = {
+    val secondStage: Stage = new Stage()
+    secondStage.initModality(Modality.APPLICATION_MODAL)
+    secondStage.initOwner(mecanicos.getScene().getWindow)
+    val fxmlLoader = new FXMLLoader(getClass.getResource("MecanicosListWindow.fxml"))
+    val mainViewRoot: Parent = fxmlLoader.load()
+    val scene = new Scene(mainViewRoot)
+    secondStage.setScene(scene)
+    secondStage.show()
+  }
+
+  def OnClientesClicked(): Unit = {
+    val thirdStage: Stage = new Stage()
+    thirdStage.initModality(Modality.APPLICATION_MODAL)
+    thirdStage.initOwner(clientes.getScene().getWindow)
+    val fxmlLoader = new FXMLLoader(getClass.getResource("ClientesWindow.fxml"))
+    val mainViewRoot: Parent = fxmlLoader.load()
+    val scene = new Scene(mainViewRoot)
+    thirdStage.setScene(scene)
+    thirdStage.show()
+  }
+
+}
