14c15a2e91c88a9c5fd3ed1a7b067b409b9f3ceb
joaob <joaobranco3000@gmail.com>
2020-12-08 18:31:32 +0000

    mec comeco das alterações

--- src/Carro.scala
+++ src/Carro.scala
@@ -20,7 +20,7 @@ case class Carro(
 
 object Carro{
 
-  def apply(modelo: String, ano: String, trabalho: Trabalho, aReparar: Boolean): Carro = new Carro(modelo, ano, trabalho, aReparar)
+  def apply(modelo: String, ano: String, dono: String, trabalho: Trabalho): Carro = new Carro(modelo, ano, dono, trabalho)
 
   //passa tempo no carro e marca como arranjado se já passou o tempo necessário
   def reparar(x: Carro):Carro ={
